b73d003d6ec9803d248c351fc27f6cb1690dfd9b
Elmar H <elmarhuseynv@gmail.com>
2023-07-19 11:11:08 +0300

    None

--- Essential_Math_for_Data_Science/Chapter_3_Descriptive_and_Inferential_Statistics/Median.py
+++ Essential_Math_for_Data_Science/Chapter_3_Descriptive_and_Inferential_Statistics/Median.py
@@ -1,13 +1,14 @@
 sample = [1, 2, 3, 4, 5]
 
 def median(values):
-    ordered = sorted(values)
-
-    n = len(values)
-    mid = int(n/2) - 1 if n%2==0 else int(n%2)
-    if n%2 == 0:
-        return(ordered[mid] + ordered[mid+1])
-    else: 
-        return ordered[mid]
     
+    ordered = sorted(values)
+    print(ordered)
+    n = len(ordered)
+    mid = int(n / 2) - 1 if n % 2 == 0 else int(n/2)
+
+    if n % 2 == 0:
+        return (ordered[mid] + ordered[mid+1]) / 2.0
+    else:
+        return ordered[mid]
 print(median(sample))
